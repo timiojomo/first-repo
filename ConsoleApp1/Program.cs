416b577799b3a77a9eb847480de252394eef4978
Timi Ojomo <timilehinojomo@gmail.com>
2026-09-03 11:29:21 +0100

    variables test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int age = 20;
+age = 25;
+float height = 5.7f;
+double weight = 70.5;
+char gender = 'M';
+string name = "Alex Bean";
+bool isStudent = true;
+Console.WriteLine(isStudent);
--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+193d99d06ad588ee5aea10198b99682996f394c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af837f4f4b119f6b96177f13b06dd57e4c8f1956")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
