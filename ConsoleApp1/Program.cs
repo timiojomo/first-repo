88d6032351870b6503898169eba7cad3e9f8290d
Timi Ojomo <timilehinojomo@gmail.com>
2026-09-07 06:10:29 +0100

    Foreach Loop

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,5 @@
-﻿int i = 1;
-do 
+﻿int[] numbers = { 1, 2, 3, 4, 5 };
+foreach (int number in numbers)
 {
-    Console.WriteLine(i);
-    i++;
-} while (i <= 10);
+    Console.WriteLine(number);
+}
--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61516d44ec09c37fe69c5d54160852366f09e702")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eed8a703c4418bfbf22f974a3ec6aa2825390c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
