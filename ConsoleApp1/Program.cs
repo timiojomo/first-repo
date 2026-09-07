7583162d0d6ffef8e9379fe7c789bbc9df534906
Timi Ojomo <timilehinojomo@gmail.com>
2026-09-07 07:26:35 +0100

    Arrays

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,27 +1,21 @@
-﻿static void ChangeValue(ref int x)
+﻿int[] numbers = { 10, 20, 30, 40, 50 };
+Console.WriteLine(numbers[2]);
+
+for(int i = 0; i < numbers.Length; i++)
 {
-    x = 20;
+    Console.WriteLine(numbers[i]/10);
 }
 
-static void ReferenceNumber()
+foreach(int number in numbers)
 {
-    int num = 10;
-    ChangeValue(ref num);
-    Console.WriteLine(num);
+    Console.WriteLine(number * 2);
 }
 
-ReferenceNumber();
+// 2D array
+int[,] matrix = 
+{ 
+    { 1, 2, 3 }, 
+    { 4, 5, 6 } 
+};
 
-static void GetValue(out int x)
-{
-    x = 30;
-}
-
-static void OutNumber()
-{
-    int num;
-    GetValue(out num);
-    Console.WriteLine(num);
-}
-
-OutNumber();
+Console.WriteLine(matrix[0, 1]);
--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8f50418a48b0abed5d0252a819efc492f698a6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df57b10f90cd8e76deeb9a8d84facd6c2507ef1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
