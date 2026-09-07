e8f50418a48b0abed5d0252a819efc492f698a6b
Timi Ojomo <timilehinojomo@gmail.com>
2026-09-07 06:30:34 +0100

    Methods

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,23 @@
-﻿int[] numbers = { 1, 2, 3, 4, 5 };
-foreach (int number in numbers)
+﻿static void Greet(string name = "User")
 {
-    Console.WriteLine(number);
+    Console.WriteLine($"Hello, {name}! Welcome to the program!");
 }
+
+Greet();
+Greet("Joan");
+
+static void Add(int a, int b)
+{
+    int sum = a + b;
+    Console.WriteLine($"{a} + {b} = {sum}");
+}
+
+Add(5, 9);
+
+static int Multiply(int a, int b)
+{
+    return a * b;
+}
+
+int result = Multiply(4, 7);
+Console.WriteLine($"4 x 7 = {result}");
--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eed8a703c4418bfbf22f974a3ec6aa2825390c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d6032351870b6503898169eba7cad3e9f8290d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
